9ed85013af6a4e854c8992d05dc8e285deb5ba89
Felipe Kuhne <fkuhne@dbserver.com.br>
2021-02-28 00:00:04 -0300

    Adding multi cores and finalizing first version.

--- twoCores.cpp
+++ twoCores.cpp
@@ -1,25 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "pico/unique_id.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
+#include "pico/multicore.h"
 
+/* Main task for second core. */
+void core1_main()
+{
+    const uint LED_PIN = 25;
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    /* Note that the delay time below is not previously defined. This is just
+     *   add randomness in repeat period just to visualize better the 
+     *   independence between the tasks of core 0 (main) and core 1 (this). */
+
+    while (true)    
+    {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(rand() >> 23);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(rand() >> 22);
+    }
+
+    return;
+}
+
+/* Reference:
+ *   pico-examples/system/unique_board_id
+ */
 void printBoardID()
 {
     pico_unique_board_id_t board_id;
     pico_get_unique_board_id(&board_id);
 
     for (int i = 0; i < PICO_UNIQUE_BOARD_ID_SIZE_BYTES; ++i)
-        printf(" %02x", board_id.id[i]);
-
-    printf("\n");
+        printf("%02x", board_id.id[i]);
 }
 
-/* References for this implementation:
+/* References:
  * raspberry-pi-pico-c-sdk.pdf, page 68. 
  * pico-examples/adc/adc_console/adc_console.c
  * https://www.twilio.com/blog/programming-raspberry-pi-pico-microcontroller-micropython */
-const float readOnboardTemperature(const char unit='C')
+const float readOnboardTemperature(const char unit)
 {
     // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
     const float conversionFactor = 3.3f / (1 << 12);
@@ -38,29 +64,26 @@ const float readOnboardTemperature(const char unit='C')
 
 int main() 
 {
-    const uint LED_PIN = 25;
-
     stdio_init_all();
     adc_init();
     adc_set_temp_sensor_enabled(true);
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    /* This is just to make time for the user to open its serial monitor. 
+     *   Otherwise the UID will not be seen. */
+    sleep_ms(10000);
 
-    printf("Hello! ;)\n");
-    printf("I am RPi Pico UID ");
+    printf("\n\nHello! I am RPi Pico UID ");
     printBoardID();
+    printf("\n\n");
 
-    while (true)    
+    /* Start the second core and its task. */
+    multicore_launch_core1(core1_main);
+
+    /* Continue with the main core's recurrent task. */
+    while (true)
     {
-        gpio_put(LED_PIN, 1);
-
-        printf("Temp = %.02f\n", readOnboardTemperature('C'));
-
-        sleep_ms(10);
-    
-        gpio_put(LED_PIN, 0);
-        sleep_ms(990);
+        printf("Temp: %.02f C\n", readOnboardTemperature('C'));
+        sleep_ms(1000);
     }
 
     return 0;
